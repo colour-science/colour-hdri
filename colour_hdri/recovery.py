63adf9161c1cb4531de515a09381cf68d671bada
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-10-21 23:28:42 +1300

    Implement highlights recovery blend method.

--- colour_hdri/recovery.py
+++ colour_hdri/recovery.py
@@ -12,7 +12,8 @@ __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
 
-__all__ = ['highlights_recovery_clip']
+__all__ = ['highlights_recovery_clip',
+           'highlights_recovery_blend']
 
 
 def highlights_recovery_clip(RGB, whitepoint, threshold=0.95):
@@ -22,3 +23,27 @@ def highlights_recovery_clip(RGB, whitepoint, threshold=0.95):
     RGB[np.any(RGB >= threshold, axis=2)] = whitepoint
 
     return RGB
+
+
+def highlights_recovery_blend(RGB, white_level, threshold=0.95):
+    M = np.array([[1, 1, 1],
+                  [1.7320508, -1.7320508, 0],
+                  [-1, -1, 2]])
+
+    clipping_level = white_level * threshold
+
+    Lab = colour.dot_vector(M, RGB)
+
+    Lab_c = colour.dot_vector(M, np.minimum(RGB, clipping_level))
+
+    s = np.sum((Lab * Lab)[..., 1:3], axis=2)
+    s_c = np.sum((Lab_c * Lab_c)[..., 1:3], axis=2)
+
+    ratio = np.sqrt(s_c / s)
+    ratio[np.logical_or(np.isnan(ratio), np.isinf(ratio))] = 1
+
+    Lab[:, :, 1:3] *= np.rollaxis(ratio[np.newaxis], 0, 3)
+
+    RGB_o = colour.dot_vector(np.linalg.inv(M), Lab)
+
+    return RGB_o
