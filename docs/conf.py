e7b396697c88016ee50c934bf0e0d0c42119fd91
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-12-15 19:18:58 +1300

    Mock "colour.RGB_COLOURSPACES" attribute in "conf.py" file.

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,9 @@ MOCK_MODULES = (
 for module in MOCK_MODULES:
     sys.modules[module] = mock.Mock()
 
+# Mock attributes for *readthedocs.org*.
+sys.modules['colour'].RGB_COLOURSPACES = {'sRGB': None}
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
