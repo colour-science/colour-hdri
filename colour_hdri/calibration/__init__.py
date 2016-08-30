d35953fc245155967786e00136b526c87dd76a83
Thomas Mansencal <thomas.mansencal@gmail.com>
2016-08-30 20:23:43 +1200

    Add support for absolute luminance calibration.

--- colour_hdri/calibration/__init__.py
+++ colour_hdri/calibration/__init__.py
@@ -3,6 +3,11 @@
 
 from __future__ import absolute_import
 
+from .absolute_luminance import (
+    absolute_luminance_calibration,
+    upper_hemisphere_illuminance_weights)
 from .debevec1997 import g_solve, camera_response_functions_Debevec1997
 
-__all__ = ['g_solve', 'camera_response_functions_Debevec1997']
+__all__ = ['absolute_luminance_calibration',
+           'upper_hemisphere_illuminance_weights']
+__all__ += ['g_solve', 'camera_response_functions_Debevec1997']
