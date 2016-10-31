cac12a3d8be65483bd3eba2c72d5a68912962ae8
Thomas Mansencal <thomas.mansencal@gmail.com>
2016-10-31 18:50:08 +1300

    Code formatting.

--- colour_hdri/process/dng.py
+++ colour_hdri/process/dng.py
@@ -285,8 +285,8 @@ def read_dng_files_exif_tags(dng_files,
     for dng_file in dng_files:
         exif_tags = read_exif_tags(dng_file)
         binding = deepcopy(exif_tags_binding)
-        for group in binding:
-            for tag in binding[group]:
+        for group, tags in binding.items():
+            for tag in tags:
                 exif_tag = exif_tags[group].get(tag)
                 parser = binding[group][tag][0]
                 if exif_tag is None:
