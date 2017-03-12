01863190061ff92bd1a78080e27234224d8353de
Thomas Mansencal <thomas.mansencal@gmail.com>
2017-03-12 19:17:38 +1300

    Update "setup.py" file.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ SHORT_DESCRIPTION = 'Colour - HDRI'
 
 LONG_DESCRIPTION = open('README.rst').read()
 
-INSTALLATION_REQUIREMENTS = ['colour-science>=0.3.8',
+INSTALLATION_REQUIREMENTS = ['colour-science>=0.3.9',
                              'recordclass>=0.4']
 
 OPTIONAL_REQUIREMENTS = ['rawpy>=0.5.0']
@@ -46,7 +46,7 @@ TESTS_REQUIREMENTS = ['coverage>=3.7.1',
                       'nose>=1.3.4']
 
 if os.environ.get('READTHEDOCS') == 'True':
-    INSTALLATION_REQUIREMENTS += ['mock==1.0.1']
+    INSTALLATION_REQUIREMENTS += ['mock']
 
 setup(name='colour-hdri',
       version='0.1.1',
