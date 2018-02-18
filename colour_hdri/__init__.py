668a56c2825e97fcef8304e1fe8769805ddd63da
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-02-19 08:53:17 +1300

    Raise package version to 0.1.4.

--- colour_hdri/__init__.py
+++ colour_hdri/__init__.py
@@ -119,7 +119,7 @@ __application_name__ = 'Colour - HDRI'
 
 __major_version__ = '0'
 __minor_version__ = '1'
-__change_version__ = '3'
+__change_version__ = '4'
 __version__ = '.'.join(
     (__major_version__,
      __minor_version__,
