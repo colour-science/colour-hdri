Fix exposure value computation in "colour_hdri.exposure_value" definition.

colour_hdri/utilities/exposure.py
@@ -5,10 +5,10 @@ Image Exposure Value Computation
 
 Defines image exposure value computation objects:
 
--   :func:`colour_hdri.exposure_value`
--   :func:`colour_hdri.adjust_exposure`
 -   :func:`colour_hdri.average_luminance`
 -   :func:`colour_hdri.average_illuminance`
+-   :func:`colour_hdri.exposure_value`
+-   :func:`colour_hdri.adjust_exposure`
 
 References
 ----------
@@ -31,70 +31,9 @@ __email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
 
 __all__ = [
-    'exposure_value', 'adjust_exposure', 'average_luminance',
-    'average_illuminance'
+    'average_luminance', 'average_illuminance', 'exposure_value',
+    'adjust_exposure'
 ]
-
-
-def exposure_value(f_number, exposure_time, iso):
-    """
-    Computes the exposure value from given image *FNumber*, *Exposure Time* and
-    *ISO* values.
-
-    Parameters
-    ----------
-    f_number : array_like
-        Image *F-Number*.
-    exposure_time : array_like
-        Image *Exposure Time*.
-    iso : array_like
-        Image *ISO*.
-
-    Returns
-    -------
-    ndarray
-        Image exposure value.
-
-    Examples
-    --------
-    >>> exposure_value(8, 1, 100)
-    6.0
-    """
-
-    N = as_float_array(f_number)
-    t = as_float_array(exposure_time)
-    S = as_float_array(iso)
-
-    EV = np.log2(N ** 2) + np.log2(1 / t) - np.log2(100 / S)
-
-    return EV
-
-
-def adjust_exposure(a, EV):
-    """
-    Adjusts given array exposure using given :math:`EV` exposure value.
-
-    Parameters
-    ----------
-    a : array_like
-        Array to adjust the exposure.
-    EV : numeric
-        Exposure adjustment value.
-
-    Returns
-    -------
-    ndarray
-        Exposure adjusted array.
-
-    Examples
-    --------
-    >>> adjust_exposure(np.array([0.25, 0.5, 0.75, 1]), 1)
-    array([ 0.5,  1. ,  1.5,  2. ])
-    """
-
-    a = as_float_array(a)
-
-    return a * pow(2, EV)
 
 
 def average_luminance(f_number, exposure_time, iso, k=12.5):
@@ -177,3 +116,66 @@ def average_illuminance(f_number, exposure_time, iso, c=250):
     E = N ** 2 / t / S * c
 
     return E
+
+
+def exposure_value(f_number, exposure_time, iso, k=12.5):
+    """
+    Computes the average illuminance in :math:`Lux` from given
+    image *F-Number* :math:`N`, *Exposure Time* :math:`t` and *ISO* speed
+    :math:`S` and *reflected light calibration constant* :math:`k`.
+
+    Parameters
+    ----------
+    f_number : array_like
+        Image *F-Number*  :math:`N`.
+    exposure_time : array_like
+        Image *Exposure Time* :math:`t`.
+    iso : array_like
+        Image *ISO* :math:`S`.
+    k : numeric, optional
+        Reflected light calibration constant :math:`k`.
+
+    Returns
+    -------
+    ndarray
+        Image exposure value.
+
+    Examples
+    --------
+    >>> exposure_value(8, 1, 100)
+    6.0
+    """
+
+    L = average_luminance(f_number, exposure_time, iso, k)
+    S = as_float_array(iso)
+
+    EV = np.log2(L * S / k)
+
+    return EV
+
+
+def adjust_exposure(a, EV):
+    """
+    Adjusts given array exposure using given :math:`EV` exposure value.
+
+    Parameters
+    ----------
+    a : array_like
+        Array to adjust the exposure.
+    EV : numeric
+        Exposure adjustment value.
+
+    Returns
+    -------
+    ndarray
+        Exposure adjusted array.
+
+    Examples
+    --------
+    >>> adjust_exposure(np.array([0.25, 0.5, 0.75, 1]), 1)
+    array([ 0.5,  1. ,  1.5,  2. ])
+    """
+
+    a = as_float_array(a)
+
+    return a * pow(2, EV)

colour_hdri/utilities/tests/test_exposure.py
@@ -9,8 +9,8 @@ from __future__ import division, unicode_literals
 import numpy as np
 import unittest
 
-from colour_hdri.utilities import (exposure_value, adjust_exposure,
-                                   average_luminance, average_illuminance)
+from colour_hdri.utilities import (average_luminance, average_illuminance,
+                                   exposure_value, adjust_exposure)
 
 __author__ = 'Colour Developers'
 __copyright__ = 'Copyright (C) 2015-2018 - Colour Developers'
@@ -20,47 +20,9 @@ __email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
 
 __all__ = [
-    'TestExposureValue', 'TestAdjustExposure', 'TestAverageLuminance',
-    'TestAverageIlluminance'
+    'TestAverageLuminance', 'TestAverageIlluminance', 'TestExposureValue',
+    'TestAdjustExposure'
 ]
-
-
-class TestExposureValue(unittest.TestCase):
-    """
-    Defines :func:`colour_hdri.utilities.exposure.exposure_value` definition
-    unit tests methods.
-    """
-
-    def test_exposure_value(self):
-        """
-        Tests :func:`colour_hdri.utilities.exposure.exposure_value` definition.
-        """
-
-        np.testing.assert_almost_equal(
-            exposure_value(
-                np.array([2.8, 5.6, 8]),
-                np.array([0.125, 0.5, 1.0]),
-                np.array([100, 800, 16000])),
-            np.array([5.97085365, 8.97085365, 13.32192809]),
-            decimal=7)  # yapf: disable
-
-
-class TestAdjustExposure(unittest.TestCase):
-    """
-    Defines :func:`colour_hdri.utilities.exposure.adjust_exposure` definition
-    unit tests methods.
-    """
-
-    def test_adjust_exposure(self):
-        """
-        Tests :func:`colour_hdri.utilities.exposure.adjust_exposure`
-        definition.
-        """
-
-        np.testing.assert_almost_equal(
-            adjust_exposure(np.array([0.25, 0.5, 0.75, 1]), 1),
-            np.array([0.5, 1.0, 1.5, 2.]),
-            decimal=7)
 
 
 class TestAverageLuminance(unittest.TestCase):
@@ -104,6 +66,45 @@ class TestAverageIlluminance(unittest.TestCase):
                 np.array([100, 800, 16000]),
             ),
             np.array([156.80000000, 19.60000000, 1.00000000]),
+            decimal=7)
+
+
+class TestExposureValue(unittest.TestCase):
+    """
+    Defines :func:`colour_hdri.utilities.exposure.exposure_value` definition
+    unit tests methods.
+    """
+
+    def test_exposure_value(self):
+        """
+        Tests :func:`colour_hdri.utilities.exposure.exposure_value` definition.
+        """
+
+        np.testing.assert_almost_equal(
+            exposure_value(
+                np.array([2.8, 5.6, 8]),
+                np.array([0.125, 0.5, 1.0]),
+                np.array([100, 800, 16000]),
+            ),
+            np.array([5.97085365, 5.97085365, 6.00000000]),
+            decimal=7)
+
+
+class TestAdjustExposure(unittest.TestCase):
+    """
+    Defines :func:`colour_hdri.utilities.exposure.adjust_exposure` definition
+    unit tests methods.
+    """
+
+    def test_adjust_exposure(self):
+        """
+        Tests :func:`colour_hdri.utilities.exposure.adjust_exposure`
+        definition.
+        """
+
+        np.testing.assert_almost_equal(
+            adjust_exposure(np.array([0.25, 0.5, 0.75, 1]), 1),
+            np.array([0.5, 1.0, 1.5, 2.]),
             decimal=7)
 
 