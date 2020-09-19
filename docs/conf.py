7a92d93802f14aa83ceac3de833d38ee76b60164
Thomas Mansencal <thomas.mansencal@gmail.com>
2020-09-19 21:44:40 +1200

    Fix documentation build issues related to class attributes and methods.

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,15 @@ import colour_hdri as package
 basename = re.sub('_(\\w)', lambda x: x.group(1).upper(),
                   package.__name__.title()).replace('Hdri', 'HDRI')
 
-autosummary_generate = True
-
+autodoc_member_order = 'bysource'
 autodoc_mock_imports = [
     'colour', 'colour.plotting', 'matplotlib', 'matplotlib.pyplot',
     'matplotlib.ticker'
 ]
+
+autosummary_generate = True
+
+napoleon_custom_sections = ['Attributes', 'Methods']
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
