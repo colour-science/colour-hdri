1886d1fce84e89550ec2bed142f9548d1dafd479
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-08-28 18:05:06 +1200

    Update various pragma statements.

--- colour_hdri/__init__.py
+++ colour_hdri/__init__.py
@@ -272,7 +272,7 @@ __version__ = ".".join(
 try:
     _version: str = (
         subprocess.check_output(
-            ["git", "describe"],  # noqa: S603,S607
+            ["git", "describe"],  # noqa: S603, S607
             cwd=os.path.dirname(__file__),
             stderr=subprocess.STDOUT,
         )
